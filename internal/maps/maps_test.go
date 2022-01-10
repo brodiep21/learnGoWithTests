706cfd7803e0e0ec41a5a459d4c844e52999b6cb
Brodie <brodiep21@hotmail.com>
2022-01-10 15:42:26 -0500

    Update

--- internal/maps/maps_test.go
+++ internal/maps/maps_test.go
@@ -76,14 +76,36 @@ func assertDefinition(t testing.TB, dictionary Dictionary, word, definition stri
 func TestUpdate(t *testing.T) {
 
 	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		newDefinition := "new definition"
 
+		err := dictionary.Update(word, newDefinition)
+
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, newDefinition)
 	})
+
+	t.Run("new word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{}
+
+		err := dictionary.Update(word, definition)
+
+		assertError(t, err, ErrWordDoesNotExist)
+	})
+}
+
+func TestDelete(t *testing.T) {
 	word := "test"
-	definition := "this is just a test"
-	dictionary := Dictionary{word: definition}
-	newDefinition := "new definition"
+	dictionary := Dictionary{word: "test definition"}
 
-	dictionary.Update(word, newDefinition)
+	dictionary.Delete(word)
 
-	assertDefinition(t, dictionary, word, newDefinition)
+	_, err := dictionary.Search(word)
+	if err != ErrNotFound {
+		t.Errorf("Expected %q to be deleted", word)
+	}
 }
